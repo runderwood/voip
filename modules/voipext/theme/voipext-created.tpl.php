f211279b200b6a4b9154da3737c1c48cd0982fe5
Bryan Hirsch <bryan@bryanhirsch.com>
2010-04-08 13:03:34 -0400

    Change Voip Module name from voipextension to voipext.

--- modules/voipext/theme/voipext-created.tpl.php
+++ modules/voipext/theme/voipext-created.tpl.php
@@ -1,18 +1,25 @@
 <?php
 /**
- * @file voipextension.tpl.php
- *   Default template for voip extension nodes ($node->content).
+ * @file voipext-created.tpl.php
+ *   Default template for voip extension info presented by drupal_set_message().
+ * 
+ *  This template is used when nodes are automatically created upon creation of 
+ *  new voip-enabled nodes.
  *
  * Available variables: 
  * - $nid, extension node's nid
- * - $ext, Extension Number X (in teaser view, this is a link to the node)
+ * - $ext, Extension Number X (this is a link to the node, except when viewed as a page)
  * - $related_node, link to related node 
  * - $related_user, link to related user
  * - $r_type, related type (type of node or "user")
  * - $is_active, extension status
  * - $r_script, link to related script node
+ * 
+ * - $edit, edit link
+ * - $msg, message to user about new extension
  */
 ?>
+<div class='voipext notification'><?php print $msg ?></div>
 <div class='voipext ext'><?php print $ext ?></div>
   <?php if ($related_node) :?>
     <div class='voipext related-node'>Related Node: <?php print $related_node ?></div>
@@ -22,3 +29,4 @@
 <div class='voipext related-type'>Type: <?php print $r_type ?></div>
 <div class='voipext ext-status'>Status: <?php print $is_active ?></div>
 <div class='voipext script'>Script: <?php print $r_script ?></div>
+<div class='voipext edit'><?php print $edit ?></div>
--- modules/voipext/theme/voipext.tpl.php
+++ modules/voipext/theme/voipext.tpl.php
@@ -1,25 +1,18 @@
 <?php
 /**
- * @file voipextension-created.tpl.php
- *   Default template for voip extension info presented by drupal_set_message().
- * 
- *  This template is used when nodes are automatically created upon creation of 
- *  new voip-enabled nodes.
+ * @file voipext.tpl.php
+ *   Default template for voip extension nodes ($node->content).
  *
  * Available variables: 
  * - $nid, extension node's nid
- * - $ext, Extension Number X (this is a link to the node, except when viewed as a page)
+ * - $ext, Extension Number X (in teaser view, this is a link to the node)
  * - $related_node, link to related node 
  * - $related_user, link to related user
  * - $r_type, related type (type of node or "user")
  * - $is_active, extension status
  * - $r_script, link to related script node
- * 
- * - $edit, edit link
- * - $msg, message to user about new extension
  */
 ?>
-<div class='voipext notification'><?php print $msg ?></div>
 <div class='voipext ext'><?php print $ext ?></div>
   <?php if ($related_node) :?>
     <div class='voipext related-node'>Related Node: <?php print $related_node ?></div>
@@ -29,4 +22,3 @@
 <div class='voipext related-type'>Type: <?php print $r_type ?></div>
 <div class='voipext ext-status'>Status: <?php print $is_active ?></div>
 <div class='voipext script'>Script: <?php print $r_script ?></div>
-<div class='voipext edit'><?php print $edit ?></div>
