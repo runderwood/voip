5cceaeca822f90d3feb623d360afe4fc5f4db30c
Leo Burd <leoburd@media.mit.edu>
2010-06-14 11:07:38 -0400

    working on weather report example

--- modules/voiptest/weather.php
+++ modules/voiptest/weather.php
@@ -1,25 +1,46 @@
 <?php
-$weather_feed = file_get_contents("http://weather.yahooapis.com/forecastrss?p=14607&u=f");
-$weather = simplexml_load_string($weather_feed);
-if(!$weather) die('weather failed');
-$copyright = $weather->channel->copyright;
+/**
+ * Retrieves weather foreacast from Yahoo Weather
+ *
+ *  Note: The following function is based on http://pkarl.com/articles/parse-yahoo-weather-rss-using-php-and-simplexml-al/
+ *
+ * @param $zip_code, the 5-digit zip code of the desired location
+ * 
+ * @param $unit, string with either 'c' for Celsius or 'f' for Fahrenheit
+ */
+function _get_weather($zip_code, $unit='f') {
+  $yahoo_url = "http://weather.yahooapis.com/forecastrss?p=$zip_code&u=$unit";
+  $weather_feed = file_get_contents($yahoo_url);
+  $weather = simplexml_load_string($weather_feed);
 
-$channel_yweather = $weather->channel->children("http://xml.weather.yahoo.com/ns/rss/1.0");
+  if(!$weather) {
+    return FALSE;
+  }
 
-foreach($channel_yweather as $x => $channel_item) 
-	foreach($channel_item->attributes() as $k => $attr) 
-		$yw_channel[$x][$k] = $attr;
+  $copyright = $weather->channel->copyright;
+  
+  $channel_yweather = $weather->channel->children("http://xml.weather.yahoo.com/ns/rss/1.0");
+  
+  foreach($channel_yweather as $x => $channel_item) 
+    foreach($channel_item->attributes() as $k => $attr) 
+		  $yw_channel[$x][$k] = $attr;
 
-$item_yweather = $weather->channel->item->children("http://xml.weather.yahoo.com/ns/rss/1.0");
+  $item_yweather = $weather->channel->item->children("http://xml.weather.yahoo.com/ns/rss/1.0");
 
-foreach($item_yweather as $x => $yw_item) {
-	foreach($yw_item->attributes() as $k => $attr) {
-		if($k == 'day') $day = $attr;
-		if($x == 'forecast') { $yw_forecast[$x][$day . ''][$k] = $attr;	} 
-		else { $yw_forecast[$x][$k] = $attr; }
-	}
+  foreach($item_yweather as $x => $yw_item) {
+    foreach($yw_item->attributes() as $k => $attr) {
+      if($k == 'day') $day = $attr;
+      if($x == 'forecast') { $yw_forecast[$x][$day . ''][$k] = $attr;	} 
+      else { $yw_forecast[$x][$k] = $attr; }
+    }
+  }
+ 
+  return $yw_forecast;
 }
 
-var_dump($yw_forecast)
+$zip = '02478';
+$unit = 'f';
+$forecast = _get_weather($zip, $unit);
+var_dump($forecast)
 ?>
 
