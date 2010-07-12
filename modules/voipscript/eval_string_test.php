45b018ab09bc882960a3df28f18cd43ca498239d
Leo Burd <leoburd@media.mit.edu>
2010-07-12 18:54:00 -0400

    trying to fix the quotes challenge

--- modules/voipscript/eval_string_test.php
+++ modules/voipscript/eval_string_test.php
@@ -2,54 +2,6 @@
 
 require_once('voipscript.inc');        
 
-function expand_variables($string, $variables, $add_quotes=NULL) {
-  $pattern = "/%(\w+)/e";
-
-  if(is_null($add_quotes)){
-    $add_quotes = ($string[0] == '^') ? TRUE : FALSE;
-  }
-  $add_quotes = ($add_quotes)? 'TRUE' : 'FALSE';
-  $replacement = "_replace_variable(\$1, \$variables, \$add_quotes);" 
-
-  $val = preg_replace($pattern, $replacement, $string);
-  $string = $val;
-//watchdog('voipscript', "val: $val");
-echo("\nval: $val\n");
-
-    //check if $val contains a function or expression to be processed
-    if($val[0] == '^') {
-      $expression = substr($val,1);
-      $val = eval("return ($expression);");
-//watchdog('voipscript',"\nexpression: return ($expression); output: $output\n");
-echo("\nexpression: return ($expression); output: $output\n");
-    }
-    return $val;
-
-}
-
-function _replace_variable($var_name, $variables, $add_quotes) {
-echo("\nin _replace_variable($var_name, $variables, $add_quotes)\n");
-$replacement = 'bla';
-/****
-  $var_value = $variables[$var_name];
-
-  if(!$var_value) {
-    $replacement = 'NULL';
-  }
-  else if($var_value[0] == '^') {
-    $replacement = expand_variables($value, $variables, $add_quotes);
-  }
-  else if($add_quotes) {
-    $replacement = "'\"' . $var_value . '\"'";
-  }
-  else {
-    $replacement = $var_value;
-  }
-
-*****/
-  return $replacement;
-}
-
 $s = new VoipScript('test');
 $s->setVar('a', 'This is a string');
 $s->setVar('b', '123');
