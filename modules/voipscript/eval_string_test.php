a9db5dfaea0c449405f194478977edc47e2a7024
Leo Burd <leoburd@media.mit.edu>
2010-07-14 21:40:54 -0400

    testing script written by stackoverflow.com contributor

--- modules/voipscript/eval_string_test.php
+++ modules/voipscript/eval_string_test.php
@@ -1,43 +1,73 @@
 <?php
 
-require_once('voipscript.inc');        
-
-$s = new VoipScript('test');
-$s->setVar('a', 'This is a string');
-$s->setVar('b', '123');
-$s->setVar('d', '%c');
-$s->setVar('e', '^5 + %d');
-$s->setVar('f', '^11 + %e');
-$s->setVar('g', '^date(\'l\')');
-$s->setVar('h', 'Today is %g.');
-
-$str = '^1 + %c';
-$rc = $s->evalString($str);
-echo("\neval $str: $rc\n");
-
-$str = '^%a != NULL';
-$rc = $s->evalString($str);
-echo("\neval $str: $rc\n");
-
-$str = '^3+%f + 3';
-$rc = $s->evalString($str);
-echo("\neval $str: $rc\n");
-
-$str = '%h';
-$rc = $s->evalString($str);
-echo("\neval $str: $rc\n");
-
-echo("\n--- New test ---\n");
-$s->setVar('input_digits','*****');
-$s->setVar('zip_code','%input_digits');
-$str = 'Say: %zip_code';
-$rc = $s->evalString($str);
-echo("\neval $str: $rc\n");
-$str = '^get_forecast(%zip_code)';
-$rc = $s->evalString($str);
-echo("\neval $str: $rc\n");
-
-
-function get_forecast($str) {
-  return 'la la la';
+function _expand_variables($str, $vars) {
+    $eval = substr($str, 0, 1) == '^';
+    $regex = '/\%(\w+)/e';
+    $replacement = "_replace_variable(\$1, \$vars, \$eval);" ;
+    $res = preg_replace($regex, $replacement ,$str);
+    if($eval) {
+        ob_start();
+        $expr = substr($res, 1);
+        if(eval('$res = ' . $expr . ';')===false) {
+            ob_end_clean();
+            die('Invalid PHP-Expression: '.$expr);
+//            watchdog('voipscript', 'Invalid PHP expression: @expr', array('@expr' => $expr), WATCHDOG_ERROR);
+        }
+        ob_end_clean();
+    }
+    return $res;
 }
+
+function _replace_variable($var_name, $vars, $eval) {
+  if(isset($vars[$var_name])) {
+    $expanded = _expand_variables($vars[$var_name], $vars);
+      if($eval) {
+        // Special handling since $str is going to be evaluated ..
+        if(!is_numeric($expanded) || (substr($expanded . '', 0, 1)==='0'
+          && strpos($expanded . '', '.')===false)) {
+          $expanded = "'$expanded'";
+      }
+    }
+      return $expanded;
+  } else {
+    // Variable does not exist in $vars array
+    if($eval) {
+      return 'null';
+    }
+//  return "$var_name";
+    return '';
+  }
+}
+
+
+$vars['a'] = 'This is a string';
+$vars['b'] = '0123';
+    $vars['d'] = '%c';
+    $vars['e'] = '^5 + %d';
+    $vars['f'] = '^11 + %e + %b*2';
+    $vars['g'] = '^date(\'l\')';
+    $vars['h'] = 'Today is %g.';
+    $vars['input_digits'] = '*****';
+    $vars['code'] = '%input_digits';
+$vars['i'] = 'Zip: %j';
+$vars['j'] = "01234";
+
+echo _expand_variables('^1 + %c',$vars);
+echo("\n");
+echo _expand_variables('^%a != NULL',$vars);
+echo("\n");
+echo _expand_variables('^3+%f + 3',$vars);
+echo("\n");
+echo _expand_variables('%h',$vars);
+echo("\n");
+echo _expand_variables('Your code is: %code',$vars);
+echo("\n");
+echo _expand_variables('%i', $vars);
+echo("\n");
+echo _expand_variables('Some info: %i', $vars);
+echo("\n");
+echo _expand_variables('My name %i is %d', $vars);
+echo("\n");
+echo _expand_variables('^xyz(%i,)', $vars);
+echo("\n");
+
